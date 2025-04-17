07d8fca36ce737c06ecc34afa157872d125c7932
colinwillis2 <willic162@gmail.com>
2025-04-17 12:19:50 -0700

    Changed plugins for postcss config.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
+/** @type {import('postcss-load-config').Config} */
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {}
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
